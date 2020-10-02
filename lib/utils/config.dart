bb2243c1421d78ff5e891ad5b4b8665e4a62a0b3
tump <abcd498936590@gmail.com>
2020-10-02 11:57:35 +0800

    .

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -2,7 +2,7 @@
 String appName = "飞镖影视";
 
 // web hostUrl localhost cms port http://10.0.2.2:9999
-String hostUrl = "https://www.chahutv.com";
+String hostUrl = "http://10.0.2.2:9999";
 
 // app cur uniqueKey => uuid
 String appUniqueKey = "d07d4da9-d535-49c9-8253-8e9a1fcec99a";
